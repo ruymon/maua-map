b43af41a295ab7bc0d191f7dce7e17e68eedbfa3
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-11-05 09:44:37 -0300

    feat: creating activities routes

--- src/app/(map-layout)/page.tsx
+++ src/app/(map-layout)/page.tsx
@@ -1,4 +1,4 @@
-import {getAllEvents} from "@/lib/events";
+import { getAllEvents } from "@/lib/events";
 
 export default async function MapRootPage() {
   const data = await getAllEvents();
@@ -8,20 +8,19 @@ export default async function MapRootPage() {
   }
 
   return (
-      <div>
-        <h1>Map Root Page</h1>
+    <div>
+      <h1>Map Root Page</h1>
 
-        <p>Events:</p>
-        <ul>
-          {data.map((event) => (
-            <li key={event.id}>
-              <h2>{event.name}</h2>
-              <p>{event.description}</p>
-              <img src={event.bannerUrl} alt="Banner" />
-            </li>
-          ))}
-        </ul>
-      </div>
-
+      <p>Events:</p>
+      <ul>
+        {data.map((event) => (
+          <li key={event.id}>
+            <h2>{event.name}</h2>
+            <p>{event.description}</p>
+            <img src={event.bannerUrl} alt="Banner" />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
