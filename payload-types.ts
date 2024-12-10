2016d1210d70fb1102ec3eab051aaa6350f70133
Ruy Monteiro <ruystfrancis@gmail.com>
2024-12-10 17:16:34 -0300

    fix: nodes sizes

--- payload-types.ts
+++ payload-types.ts
@@ -17,9 +17,9 @@ export interface Config {
     rooms: Room;
     nodes: Node;
     edges: Edge;
-    'payload-locked-documents': PayloadLockedDocument;
-    'payload-preferences': PayloadPreference;
-    'payload-migrations': PayloadMigration;
+    "payload-locked-documents": PayloadLockedDocument;
+    "payload-preferences": PayloadPreference;
+    "payload-migrations": PayloadMigration;
   };
   collectionsJoins: {};
   collectionsSelect: {
@@ -29,9 +29,15 @@ export interface Config {
     rooms: RoomsSelect<false> | RoomsSelect<true>;
     nodes: NodesSelect<false> | NodesSelect<true>;
     edges: EdgesSelect<false> | EdgesSelect<true>;
-    'payload-locked-documents': PayloadLockedDocumentsSelect<false> | PayloadLockedDocumentsSelect<true>;
-    'payload-preferences': PayloadPreferencesSelect<false> | PayloadPreferencesSelect<true>;
-    'payload-migrations': PayloadMigrationsSelect<false> | PayloadMigrationsSelect<true>;
+    "payload-locked-documents":
+      | PayloadLockedDocumentsSelect<false>
+      | PayloadLockedDocumentsSelect<true>;
+    "payload-preferences":
+      | PayloadPreferencesSelect<false>
+      | PayloadPreferencesSelect<true>;
+    "payload-migrations":
+      | PayloadMigrationsSelect<false>
+      | PayloadMigrationsSelect<true>;
   };
   db: {
     defaultIDType: string;
@@ -40,7 +46,7 @@ export interface Config {
   globalsSelect: {};
   locale: null;
   user: User & {
-    collection: 'users';
+    collection: "users";
   };
   jobs: {
     tasks: unknown;
@@ -131,7 +137,7 @@ export interface Room {
  */
 export interface User {
   id: string;
-  role?: ('admin' | 'user') | null;
+  role?: ("admin" | "user") | null;
   updatedAt: string;
   createdAt: string;
   email?: string | null;
@@ -167,7 +173,7 @@ export interface Edge {
   start_node: string | Node;
   end_node: string | Node;
   cost?: number | null;
-  type?: ('crosswalk' | 'path' | 'staircase') | null;
+  type?: ("crosswalk" | "path" | "staircase") | null;
   updatedAt: string;
   createdAt: string;
 }
@@ -179,32 +185,32 @@ export interface PayloadLockedDocument {
   id: string;
   document?:
     | ({
-        relationTo: 'media';
+        relationTo: "media";
         value: string | Media;
       } | null)
     | ({
-        relationTo: 'events';
+        relationTo: "events";
         value: string | Event;
       } | null)
     | ({
-        relationTo: 'users';
+        relationTo: "users";
         value: string | User;
       } | null)
     | ({
-        relationTo: 'rooms';
+        relationTo: "rooms";
         value: string | Room;
       } | null)
     | ({
-        relationTo: 'nodes';
+        relationTo: "nodes";
         value: string | Node;
       } | null)
     | ({
-        relationTo: 'edges';
+        relationTo: "edges";
         value: string | Edge;
       } | null);
   globalSlug?: string | null;
   user: {
-    relationTo: 'users';
+    relationTo: "users";
     value: string | User;
   };
   updatedAt: string;
@@ -217,7 +223,7 @@ export interface PayloadLockedDocument {
 export interface PayloadPreference {
   id: string;
   user: {
-    relationTo: 'users';
+    relationTo: "users";
     value: string | User;
   };
   key?: string | null;
@@ -377,7 +383,6 @@ export interface Auth {
   [k: string]: unknown;
 }
 
-
-declare module 'payload' {
+declare module "payload" {
   export interface GeneratedTypes extends Config {}
 }
