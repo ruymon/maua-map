0019ec5ddec72de7d80ad6d1c3b831b2de6853c7
Ruy Monteiro <ruystfrancis@gmail.com>
2025-02-07 22:25:21 -0300

    feat: display points temporarely

--- src/app/(frontend)/(app-layout)/_components/map/map-shell.tsx
+++ src/app/(frontend)/(app-layout)/_components/map/map-shell.tsx
@@ -1,10 +1,12 @@
+import { getAllEdges } from "@/data/edges";
+import { getAllNodes } from "@/data/nodes";
 import { Map } from ".";
 
 export async function MapShell() {
-  // const [nodeData, edgeData] = await Promise.all([
-  //   getAllNodes(),
-  //   getAllEdges(),
-  // ]);
+  const [nodeData, edgeData] = await Promise.all([
+    getAllNodes(),
+    getAllEdges(),
+  ]);
 
-  return <Map />;
+  return <Map nodesData={nodeData} edgesData={edgeData} />;
 }
